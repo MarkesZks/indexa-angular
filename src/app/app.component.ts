0c2ef524eacc729396fdff3000e2ca0a3b3508be
Gabriel Marques <gabriel.marquesbiel@hotmail.com>
2024-10-01 15:32:01 -0300

    feat:Cabecalho component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./components/container/container.component";
+import { CabecalhoComponent } from "./components/cabecalho/cabecalho.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent],
+  imports: [RouterOutlet, ContainerComponent, CabecalhoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
