9b4116c779a97183d613614bafe170a153caa653
Gabriel Marques <gabriel.marquesbiel@hotmail.com>
2024-10-01 19:08:23 -0300

    feat:separador component implementando o @for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./components/container/container.component";
 import { CabecalhoComponent } from "./components/cabecalho/cabecalho.component";
+import { SeparadorComponent } from "./components/separador/separador.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent, CabecalhoComponent],
+  imports: [RouterOutlet, ContainerComponent, CabecalhoComponent, SeparadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+
+  alfabeto: string= 'abcdefghijklmnopqrstuvwxyz'
+
+  ngOnInit(): void{
+
+  }
 }
---
+++ src/app/components/separador/separador.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { toArray } from 'rxjs';
+
+@Component({
+  selector: 'app-separador',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './separador.component.html',
+  styleUrl: './separador.component.css'
+})
+export class SeparadorComponent {
+
+
+
+
+
+}
+
