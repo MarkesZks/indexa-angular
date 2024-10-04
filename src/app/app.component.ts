32896b30505f76038d090245067cd6d7f55624f7
Gabriel Marques <gabriel.marquesbiel@hotmail.com>
2024-10-04 14:48:52 -0300

    feat: style input e filtro por acento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,63 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContainerComponent } from "./components/container/container.component";
-import { CabecalhoComponent } from "./components/cabecalho/cabecalho.component";
-import { SeparadorComponent } from "./components/separador/separador.component";
+import { ContainerComponent } from './components/container/container.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './components/separador/separador.component';
 import { ContatosComponent } from './components/contatos/contatos.component';
+import { FormsModule } from '@angular/forms';
 
 interface Contato {
-  id:number
-  nome:string
-  telefone:string
+  id: number;
+  nome: string;
+  telefone: string;
 }
 
-import agenda from './agenda.json'
+import agenda from './agenda.json';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent, CabecalhoComponent, SeparadorComponent,ContatosComponent],
+  imports: [
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatosComponent,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
 
-  alfabeto: string= 'abcdefghijklmnopqrstuvwxyz'
-  contatos:Contato[] = agenda;
+  filtarPorTexto: string = '';
 
-  filtrarContatosPorLetraInicial(letra:string):Contato[]{
+  private removerAcentos(texto:string):string{
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+  }
+
+  filtrarContatosPorTexto():Contato[]{
+    // Verifica se está vazia. usuário não digitou nada no input.
+    if(!this.filtarPorTexto){
+      return this.contatos
+    }
+    //Caso tenha digitado filtra a lista de contatos
     return this.contatos.filter(contato =>{
-      return contato.nome.toLowerCase().startsWith(letra)
-    })
+      //Passa uma função para cada contato verificando se ele inclui o texto digitado
+      return this.removerAcentos(contato.nome).toLowerCase().includes(this.filtarPorTexto.toLowerCase())
+    })}
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatosPorTexto().filter(contato => {
+      return this.removerAcentos(contato.nome).toLowerCase().startsWith(letra);
+    });
+  }
 
   }
 
-  ngOnInit(): void{
 
-  }
-}
+
+
